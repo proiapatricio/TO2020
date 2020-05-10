5e31cd0a2b153c77f8c4ada69191310c9e24b0c5
RichoM <richi.moran@gmail.com>
2020-05-10 13:21:40 -0300

    3 implementaciones de Fibonacci escritas en clase (recursivo, iterativo, y memoization)

--- StopwatchExample/Program.cs
+++ StopwatchExample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace StopwatchExample
@@ -11,13 +12,39 @@ namespace StopwatchExample
             for (int i = 0; i <= 100; i++)
             {
                 sw.Restart();
-                int f = Fib(i);
+                int f = FibMemo(i);
                 sw.Stop();
                 Console.WriteLine($"{i} - {f} ({sw.ElapsedMilliseconds} ms)");
             }
         }
 
-        static int Fib(int n)
+        static Dictionary<int, int> calculatedValues = new Dictionary<int, int>();
+        static int FibMemo(int n) // Memoization
+        {
+            // 0 1 1 2 3 5 8 ...
+            if (n < 0) throw new ArgumentException("No válido para negativos");
+            if (n <= 1) return n;
+            if (calculatedValues.ContainsKey(n)) return calculatedValues[n];
+            calculatedValues[n] = FibMemo(n - 1) + FibMemo(n - 2);
+            return calculatedValues[n];
+        }
+
+        static int FibIter(int n) // Proceso iterativo
+        {
+            if (n < 0) throw new ArgumentException("No válido para negativos");
+            if (n <= 1) return n;
+            int a1 = 0;
+            int a2 = 1;
+            for (int i = 1; i < n; i++)
+            {
+                int temp = a2 + a1;
+                a1 = a2;
+                a2 = temp;
+            }
+            return a2;
+        }
+
+        static int Fib(int n) // Proceso recursivo
         {
             // 0 1 1 2 3 5 8 ...
             if (n < 0) throw new ArgumentException("No válido para negativos");
