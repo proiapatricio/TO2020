d3fe73b3e9b31c42f3177288fa890c53cf1c6909
RichoM <richi.moran@gmail.com>
2020-05-15 00:08:56 -0300

    Ejemplo de Linq

---
+++ LinqExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var numeros = GetIntegers();
+
+            var pares = numeros.Where(n =>
+            {
+                return n % 2 == 0;
+            });
+
+            var cuadradosPares = pares.Select(n => n * n);
+            var primeros100 = cuadradosPares.Take(100);
+
+            foreach (var n in primeros100)
+            {
+                Console.WriteLine(n);
+            }
+
+        }
+
+        static IEnumerable<int> GetIntegers()
+        {
+            int n = 0;
+            while(true)
+            {
+                yield return n++;
+            }
+        }
+    }
+}
